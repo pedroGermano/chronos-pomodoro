d2f048566cbf65b8af50ba016eb56aaf8ae78e4b
Pedro Germano <pedrogermano232@gmail.com>
2025-03-31 12:45:39 -0300

    header start

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import { Heading } from "./components/Heading";
 
 import "./styles/theme.css";
 import "./styles/global.css";
+import { TimerIcon } from "lucide-react";
 
 export function App() {
   return (
     <>
-      <Heading />
+      <Heading>
+        Olá mundo 1
+        <TimerIcon />
+      </Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum aliquam
         nihil quis, tempora dicta ea fugiat reiciendis ducimus tempore
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,9 @@
 import styles from "./Heading.module.css";
 
-export function Heading() {
-  return <h1 className={styles.heading}>Olá mundo!</h1>;
+type HeadingProps = {
+  children: React.ReactNode;
+};
+
+export function Heading(props: HeadingProps) {
+  return <h1 className={styles.heading}>{props.children}</h1>;
 }
