8069ff79dd43259b3a271d217ffd04f50002b55a
Pedro Germano <pedrogermano232@gmail.com>
2025-04-10 19:40:26 -0300

    cycles components

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { DefaultInput } from "./components/DefaultInput";
 
 import "./styles/theme.css";
 import "./styles/global.css";
+import { Cycles } from "./components/Cycles";
 
 export function App() {
   return (
@@ -25,7 +26,12 @@ export function App() {
       <Container>
         <form className="form" action="#">
           <div className="formRow">
-            <DefaultInput id="meuInput" type="text" />
+            <DefaultInput
+              labelText="task"
+              placeholder="Digite algo"
+              id="meuInput"
+              type="text"
+            />
           </div>
 
           <div className="formRow">
@@ -33,8 +39,7 @@ export function App() {
           </div>
 
           <div className="formRow">
-            <p>Ciclos</p>
-            <p>0 0 0 0 0 0 0</p>
+            <Cycles />
           </div>
 
           <div className="formRow">
